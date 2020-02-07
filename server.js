8c7751d11850b787a3c94d1282bc3dd2deff2bd8
Walls0123 <45473708+Walls0123@users.noreply.github.com>
2020-02-07 15:46:10 -0500

    first subida

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/AngularRefactor'));
+app.get('/',function(req,res){
+    res.render(path.join(__dirname+'/dist/AngularRefactor/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
